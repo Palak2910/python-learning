7006dfdfa9eeeb23c0b804da4084c4d3c55b5a72
palakpandey29 <pkpy2007@gmail.com>
2026-08-25 20:36:46 +0530

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,31 @@
+#integer data type
+print("integer data type")
+
+#numberic data type
+x=5
+print(x)
+
+#float data type
+x=5.5
+print(x)
+
+#complex data type
+x=2j
+print(x)
+
+#list data type
+x=["apple","orange","grapes"]
+print(x)
+
+#tuple data type
+x=("apple","orange","grapes")
+print(x)
+
+#range data type
+x=range(10)
+print(x)
+
+#boolean data type
+x=True
+print(x)
+
