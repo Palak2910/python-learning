0f1a64bbe68462b82e0105b32c8e06eda74b0119
palakpandey29 <pkpy2007@gmail.com>
2026-08-26 20:22:03 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,26 @@
+#string 
+print("hello world")
+
+#assign string to a variable
+x="hello world"
+print(x)
+
+#string are array
+#Python does not have a character data type, a single character is simply a string with a length of 1
+x = "hello world"
+print(x[0])
+print(x[1])
+
+#looping through a string 
+for x in "apple":
+    print(x)
+
+#string length
+x ="hii! my name is palak pandey"
+print(len(x))
+
+#check string 
+x = "Python is easy to learn"
+print("my" in x)
+print("easy" in x)
+
