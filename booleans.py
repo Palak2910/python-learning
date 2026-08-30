c354ab863f9237ecf05371e9d928971398f6c6f8
palakpandey29 <pkpy2007@gmail.com>
2026-08-30 19:25:52 +0530

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,29 @@
+#booleans value 
+print(2==2)
+print(2>3)
+print(2<3)
+
+a=100
+b=50
+if a>b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+
+#evaluate values and variables
+print(bool("hello"))
+print(bool(10))
+
+x= "hello"
+y= 10
+print(bool(x))
+print(bool(y))
+
+#def fun^
+def myfunction():
+    return True
+if myfunction():
+    print("yes")
+else:
+    print("no")    
+    
