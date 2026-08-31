69131c8d768900dd18058b85b4bcd67e1b7ef5f0
palakpandey29 <pkpy2007@gmail.com>
2026-08-31 20:46:24 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,26 @@
+#Arithmetic operators
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+#Logical operators
+a =10
+print(a>5 and a<15) #And operator
+print(a>5 or a<15) #Or operator
+print(not(a>5 and a<15)) #Not operator
+
+#Comparison operators
+a=20
+b=10
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
