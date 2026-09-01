56e88841abd7ceaa3861fadf1db7fee158836c7c
palakpandey29 <pkpy2007@gmail.com>
2026-09-01 20:31:26 +0530

    Add files via upload

--- operators.py
+++ operators.py
@@ -24,3 +24,85 @@ print(a>b)
 print(a<b)
 print(a>=b)
 print(a<=b)
+
+#Assignment operators
+x=10
+print(x)
+
+x=10
+x+=3
+print(x)
+
+x=10
+x-=3
+print(x)
+
+x=10
+x*=3
+print(x)
+
+x=10
+x/=3
+print(x)
+
+x=10
+x%=3
+print(x)
+
+x=10
+x//=3
+print(x)
+
+x=10
+x**=3
+print(x)
+
+x=10
+x&=3
+print(x)
+
+x=10
+x|=3
+print(x)
+
+x=10
+x^=3
+print(x)
+
+x=10
+x>>=3
+print(x)
+
+x=10
+x<<=3
+print(x)
+
+#Ternary Operator
+num = 7
+x="WEEKEND" if num>5 else "WEEKDAY"
+print(x)
+
+#Identity operators
+a=10
+b=10
+print(a is b)
+
+a="apple"
+b="banana"
+print(a is not b)
+
+#Membership operators
+a="apple"
+b="banana"
+print("a" in a)
+print("b" not in b)
+
+#Bitwise operators
+a=6
+b=3
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a<<1)
+print(a>>1)
