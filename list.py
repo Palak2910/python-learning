c86c9e470e8c4dd93c6fff698092a405ca7345f9
palakpandey29 <pkpy2007@gmail.com>
2026-09-05 16:32:04 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -67,3 +67,80 @@ print(x)
 x=["banana","cheery","apple","orange","kiwi","mango"]
 x.clear()
 print(x)
+
+#Loop through a list 
+x=["banana","cheery","apple","orange","kiwi","mango"]
+for i in x:
+    print(i)
+
+#Loop through a list using index
+x=["banana","cheery","apple","orange","kiwi","mango"]
+for i in range(len(x)):
+    print(x[i])
+
+#using while loop 
+x=["banana","cheery","apple","orange","kiwi","mango"]
+i=0
+while i<len(x):
+    print(x[i])
+    i+=1
+
+#list comprehension
+x=["banana","cheery","apple","orange","kiwi","mango"]
+newlist=[i for i in x if "a" in i]
+print(newlist)
+
+#sort list 
+x=["banana","cheery","apple","orange","kiwi","mango"]
+x.sort()
+print(x)
+
+#sort list in descending order
+x=["banana","cheery","apple","orange","kiwi","mango"]
+x.sort(reverse=True)
+print(x)
+
+#customized sort 
+x=["banana","cheery","apple","orange","kiwi","mango"]
+def myfunc(n):
+    return len(n)
+
+#reverse order
+x=["banana","cheery","apple","orange","kiwi","mango"]
+x.reverse()
+print(x)
+
+#Copy list 
+x=["banana","cheery","apple","orange","kiwi","mango"]
+y=x.copy()
+print(y)
+
+#copy list using list() method
+x=["banana","cheery","apple","orange","kiwi","mango"]
+y=list(x)
+print(y)
+
+#slice operator
+x=["banana","cheery","apple","orange","kiwi","mango"]
+print(x[2:5])
+print(x[:4])
+print(x[2:])
+
+#Join two list
+x=["banana","cheery","apple"]
+y=["orange","kiwi","mango"]
+z=x+y
+print(z)
+
+#Join two list using append() method
+x=["banana","cheery","apple"]
+y=["orange","kiwi","mango"]
+for i in y:
+    x.append(i)
+    print(x)
+
+#Join two list using extend() method
+x=["banana","cheery","apple"]
+y=["orange","kiwi","mango"]
+x.extend(y)
+print(x)
