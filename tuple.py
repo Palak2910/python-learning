a27d1f5f95d4d731daad2fc0f43f11f3641dce3d
palakpandey29 <pkpy2007@gmail.com>
2026-09-07 20:06:16 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,27 @@
+#Tuple
+x=("banana","cheery","apple","orange","kiwi","mango")
+print(x)
+
+#tuple length
+x=("banana","cheery","apple","orange","kiwi","mango")
+print(len(x))
+
+#tuple with different data types
+x=(10,"apple",True,False,3.14)
+print(x)
+
+#Access tuple items
+x=("banana","apple","mango")
+print(x[1])
+
+#negative indexing
+x=("banana","apple","mango")
+print(x[-1])
+
+#checking the item in the tuple
+x=("banana","apple","mango")
+if"apple" in x:
+    print("yes, apple is in the tuple")
+else:
+    print("no, apple is not in the tuple")
+        
