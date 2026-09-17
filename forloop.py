d535d6afccf95b3295f0d5ba566fdd1d5ca10742
palakpandey29 <pkpy2007@gmail.com>
2026-09-17 22:16:57 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,33 @@
+#create for loop
+fruits = ["apple","mango","orange"]
+for x in fruits:
+    print(x)
+
+#Looping through a string 
+for i in "banana":
+    print(x)
+
+#break statement
+fruits = ["apple","cheery","mango"]
+for x in fruits:
+    print(x)
+    if x == "mango":
+        break
+
+#continue 
+flowers = ["rose","lily","sunflower"]
+for x in flowers:
+    if x == "lily":
+        continue
+    print(x)
+
+#range function
+for i in range(6):
+    print(i)
+
+#Nested loop
+fruits = ["apple","mango","orange"]
+flowers = ["rose","lily","sunflower"]
+for x in "fruits":
+    for y in "flowers":
+        print(x,y)
